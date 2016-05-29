845ae8b92909e56baa4689df3c0ec2db37b45054
Sakarah <sakarah92@gmail.com>
2016-05-29 15:52:29 +0200

    Patch functions in Primitives (-> instead of =)

--- Primitives.ml
+++ Primitives.ml
@@ -3,20 +3,20 @@
 *)
 
 let bin_op = 
-  [| ("+", fun a b = a +. b),
-     ("-", fun a b = a -. b),
-     ("*", fun a b = a *. b),
-     ("/", fun a b = a /. b),
-     ("^", fun a b = a ** b) |]
+  [| ("+", fun a b -> a +. b);
+     ("-", fun a b -> a -. b);
+     ("*", fun a b -> a *. b);
+     ("/", fun a b -> a /. b);
+     ("^", fun a b -> a ** b) |]
 
 (*
 ** Unary primitives
 *)
 
 let un_op = 
-  [| ("cos", fun a = cos a),
-     ("sin", fun a = sin a),
-     ("tan", fun a = tan a),
-     ("ln", fun a = log a),
-     ("exp", fun a = exp a) |]
+  [| ("cos", fun a -> cos a);
+     ("sin", fun a -> sin a);
+     ("tan", fun a -> tan a);
+     ("ln", fun a -> log a);
+     ("exp", fun a -> exp a) |]
 
