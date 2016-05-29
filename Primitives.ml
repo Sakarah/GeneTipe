d1740d85f2b25e04f543e2732420ae6868fa0af2
Skodt of Deme <skodtofdeme@gmail.com>
2016-05-29 14:27:06 +0200

    Create Primitives.ml
    Primitives are displayed as compounds of string and function.

---
+++ Primitives.ml
@@ -0,0 +1,22 @@
+(*
+** Binary primitives
+*)
+
+let bin_op = 
+  [| ("+", fun a b = a +. b),
+     ("-", fun a b = a -. b),
+     ("*", fun a b = a *. b),
+     ("/", fun a b = a /. b),
+     ("^", fun a b = a ** b) |]
+
+(*
+** Unary primitives
+*)
+
+let un_op = 
+  [| ("cos", fun a = cos a),
+     ("sin", fun a = sin a),
+     ("tan", fun a = tan a),
+     ("ln", fun a = log a),
+     ("exp", fun a = exp a) |]
+
