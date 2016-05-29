d9fb6386ae852e5d316a1f84874862a656f0e1df
skodt <skodtofdeme@gmail.com>
2016-05-29 11:47:03 +0200

    added fitness function
    For the fitness function, I have taken the sum of the square differences.

--- Evolver.ml
+++ Evolver.ml
@@ -1,9 +1,15 @@
-let init_population ~size =
-    [||] (* Skodt *)
+let init_population ~size max_depth =
+    Array.make size (0. , create_random max_depth)
 ;;
 
 let fitness points dna =
-    0.0 (* Skodt *)
+    let n = Array.length points in
+    let difference = ref 0. in
+    for i = 0 to n-1 do
+        let x,y = points.(i) in
+        difference := !difference +. ( eval x dna - y ) ** 2.
+    done;
+    !difference
 ;;
 
 let tournament initialPopulation =
