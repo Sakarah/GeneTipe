ebf268987396b3647d2a14c1a4c6a629e943d94e
Sakarah <sakarah92@gmail.com>
2016-06-01 00:02:02 +0200

    Make documentation compliant with ocamldoc syntax Add documentation for randomGenParams

--- Dna.mli
+++ Dna.mli
@@ -1,44 +1,53 @@
 (** Dna.t is the type of the genetic characteristics of an individual function
     internally represented by a tree. It allows diferent kind of genetic manipulation
     like random generation, crossover or evaluation of the underlying function.
-    All functions directly manipulating DNA should be found in this module. **)
+    All functions directly manipulating DNA should be found in this module. *)
 
 type t
 exception IllFormed
 
-(** This type represents the parameters for random generation of an individual **)
+(** This type represents the parameters for random generation of an individual *)
 type randomGenParams =
 {
-    fill_proba: float;
-    bin_op:(float * string * (float -> float -> float)) array ;
-    bin_proba:float ;
-    un_op:(float * string * (float -> float)) array ;
-    un_proba:float ;
-    const_range:(float*float) ;
-    const_proba:float
-    (* The rest of the probabilities represent the choice of the variable (x) *)
+    fill_proba: float; (** Probability of the full method beeing selected for generation *)
+    bin_op:(float * string * (float -> float -> float)) array ; (** Array of all binary operations with their associated probability knowing that a binary node have been selected *)
+    bin_proba:float ; (** Probability of choosing a binary node *)
+    un_op:(float * string * (float -> float)) array ; (** Array of all unary operations with their associated probability knowing that an unary node have been selected *)
+    un_proba:float ; (** Probability of choosing an unary node *)
+    const_range:(float*float) ; (** Range where constants are randomly taken *)
+    const_proba:float (** Probability of choosing a constant. The rest of the probabilities represent the choice of the variable x *)
 }
 
-(** Randomly generate a new individual who has a depth below max_depth **)
+(** {2 Random generation} *)
+(** Randomly generate a new individual who has a depth below max_depth *)
 val create_random_grow : max_depth:int -> randomGenParams -> t
-(** Randomly generate a new individual who has a depth of exactly max_depth **)
+
+(** Randomly generate a new individual who has a depth of exactly max_depth *)
 val create_random_fill : max_depth:int -> randomGenParams -> t
-(** Randomly generate a new individual choosing between the grow or the fill method **)
+
+(** Randomly generate a new individual choosing between the grow or the fill method *)
 val create_random : max_depth:int -> randomGenParams -> t
 
-(** Generate a new individual by doing a crossover wich replace some parts of the first dna by elements of the second **)
+
+(** {2 Gene manipulation} *)
+(** Generate a new individual by doing a crossover wich replace some parts of the first dna by elements of the second *)
 val crossover : depth:int -> t -> t -> t
-(** Generate a new individual by modifying an existing individual adding him new randomly generated characteristics **)
+
+(** Generate a new individual by modifying an existing individual adding him new randomly generated characteristics *)
 val mutation : depth:int -> randomGenParams -> t -> t
-(** Generate a new individual by tweaking constants of an already existing one **)
+
+(** Generate a new individual by tweaking constants of an already existing one *)
 val mutate_constants : range:(float*float) -> proba:float -> t -> t
 
+
+(** {2 Evaluation and printing} *)
 (** Evaluate the function represented at the point x.
     If any exceptions is caught during the mathematical evaluation, the exception IllFormed will be raised.
-    It will have to be caught during the Evolver.tournament function. **)
+    It will have to be caught during the Evolver.tournament function. *)
 val eval : float -> t -> float
 
-(** Give a string representation of the DNA **)
+(** Give a string representation of the DNA *)
 val to_string : ?bracket:bool -> t -> string
-(** Print the DNA **)
+
+(** Print the DNA *)
 val print : Format.formatter -> t -> unit
--- Evolver.mli
+++ Evolver.mli
@@ -1,17 +1,18 @@
 (** Evolver contains all the functions related to creation and then evolution
     of a population. It implements natural selection mechanics and manage the
-    population growth by mutation and crossover. **)
+    population growth by mutation and crossover. *)
 
-(** Initialize the population with randomly generated individuals **)
+(** Initialize the population with randomly generated individuals *)
 val init_population : size:int -> max_depth:int -> Dna.randomGenParams -> (float option * Dna.t) array
 
-(** Measure how interesting a function is **)
+(** Measure how interesting a function is *)
 val fitness : (float*float) array -> Dna.t -> float
-(** Organize a fight between functions to discard some of the weakest **)
+
+(** Organize a fight between functions to discard some of the weakest *)
 val tournament : (float option * Dna.t) array -> (float option * Dna.t) array
 
-(** Recombine existing individuals and make mutations to create new functions **)
+(** Recombine existing individuals and make mutations to create new functions *)
 val reproduce : (float option * Dna.t) array -> (float option * Dna.t) array
 
-(** Evolve the population with the fixed number of generations **)
+(** Evolve the population with the fixed number of generations *)
 val evolve : (float option * Dna.t) array -> generations:int -> (float option * Dna.t) array
