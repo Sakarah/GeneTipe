4b9b77c3888b7d9fadbcb7a8f08c94fc107e11b3
Sakarah <sakarah92@gmail.com>
2016-06-05 21:28:53 +0200

    Print results even if Ctrl+C is pressed during evolution. No more prompts for asking answers for usage in conjuction with pipes or files.

--- genetipe.ml
+++ genetipe.ml
@@ -15,32 +15,34 @@ let () =
     let usage_msg =
         "GeneTipe is a symbolic regression tool.\n" ^
         "It automatically build a function matching the points given using a genetic algorithm.\n" ^
+        "The program waits as input the number of sampling points on the first line and then on each line the x and y coordinates of a point separated by a space.\n\n" ^
         "Options available:"
     in
 
     Arg.parse spec_list (fun _ -> raise (Arg.Bad "Unexpected anonymous argument")) usage_msg;
 
-    Printf.printf "Number of points in the sample : %!";
     let nb_points = Scanf.scanf "%d\n" (function n -> n) in
 
     let points = Array.make nb_points (0.,0.) in
-    Printf.printf "Write a point on each line in the format : x y\n%!";
     for i = 0 to nb_points-1 do
         points.(i) <- Scanf.scanf "%f %f\n" (fun x y -> (x,y))
     done;
 
-    Printf.printf "\nInitialize the population with %d individuals\n" !pop_size;
+    Printf.printf "Initialize the population with %d individuals\n" !pop_size;
     let gen_params = Parameters.rand_gen_params in
     let evo_params = Parameters.evolution_params !max_depth gen_params in
     let init_pop = Evolver.init_population ~size:!pop_size ~max_depth:!max_depth gen_params in
     let pop = ref (Evolver.compute_fitness points init_pop) in
     Stats.print_population !pop;
 
-    for g = 1 to !generations do
-        Printf.printf "- Generation %d -\n%!" g;
-        pop := Evolver.evolve points evo_params !pop;
-        Stats.print_stats !pop
-    done;
+    Sys.catch_break true; (* If you do a Ctrl+C you still have the results *)
+    (try
+        for g = 1 to !generations do
+            Printf.printf "- Generation %d -\n%!" g;
+            pop := Evolver.evolve points evo_params !pop;
+            Stats.print_stats !pop
+        done
+    with Sys.Break -> ());
 
     Printf.printf "= End of evolution =\n";
     Stats.print_population !pop
