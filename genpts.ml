c45189c33a78e396e17ecc05cce2b8b986564520
Sakarah <sakarah92@gmail.com>
2016-06-07 17:52:26 +0200

    Added genpts tool

---
+++ genpts.ml
@@ -0,0 +1,32 @@
+let () =
+    let nb_points = ref 101 in
+	let minX = ref 0. in
+	let maxX = ref 10. in
+	let funcStr = ref "" in
+	
+    let spec_list =
+    [
+		("-r", Arg.Tuple [Arg.Set_float minX ; Arg.Set_float maxX], "Set the range of the generated points (default is [0,10])");
+        ("-n", Arg.Set_int nb_points, "Set the number of points to generate (deafult is 101)")
+    ]
+    in
+
+    let usage_msg =
+        "Generate a set a points from the given function.\n" ^
+        "The program prints the number of sampling points on the first line and then on each line the x and y coordinates of a point separated by a space.\n"^
+		"Usage : genpts [options] function\n\n" ^
+        "Options available:"
+    in
+
+    Arg.parse spec_list (fun anon -> funcStr := anon) usage_msg;
+
+	let func = MathParser.parse !funcStr in
+	
+    Printf.printf "%d\n" !nb_points;
+	let step = ((!maxX)-.(!minX))/.(float_of_int (!nb_points-1)) in
+    for point = 0 to !nb_points-1 do
+		let x = !minX +. step *. (float_of_int point) in
+		let y = func x in
+		Printf.printf "%f %f\n" x y
+	done;
+;;
