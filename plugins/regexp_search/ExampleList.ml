f0bae859d46fe43ec7110cd59d1a9dde38c6869b
Sakarah <sakarah92@gmail.com>
2016-12-20 14:51:25 +0100

    Patched ExampleList for reading examples that contain spaces

--- plugins/regexp_search/ExampleList.ml
+++ plugins/regexp_search/ExampleList.ml
@@ -3,10 +3,10 @@ type t = string array * string array;;
 let id x = x;;
 
 let read_string_array () =
-    let nb_str = Scanf.scanf "%d\n" id in
+    let nb_str = Scanf.sscanf (read_line ()) "%d" id in
     let str_array = Array.make nb_str "" in
     for i = 0 to nb_str-1 do
-        str_array.(i) <- Scanf.scanf "%s\n" id
+        str_array.(i) <- read_line ()
     done;
     str_array
 ;;
