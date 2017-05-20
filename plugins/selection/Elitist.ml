ecbdab88237c6cb962c7c78dccf2c6f9896122a9
Gabzcr <gabrielle.pauvert@laposte.net>
2017-05-20 18:01:34 +0200

    corrected a warning

--- plugins/selection/Elitist.ml
+++ plugins/selection/Elitist.ml
@@ -3,7 +3,6 @@
 module ElitistMethod (Fitness : EvolParams.Fitness) =
 struct
     let f initial_population ~target_size =
-        let size = Array.length initial_population in
         let winners = Array.make target_size initial_population.(0) in
         Array.sort (fun (score1,_) (score2,_) -> Fitness.compare score1 score2)  initial_population;
         for i = 0 to (target_size - 1) do
