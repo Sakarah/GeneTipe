5c05c38ac1b44189ec6a39febdee4c10426cd555
Sakarah <sakarah92@gmail.com>
2017-05-21 20:43:46 +0200

    Replace counts by ratio in fitness evaluation for regexps This allows the parameters to adjust to multiple input size

--- plugins/regexp_search/fitness/MultiobjectiveFitness.ml
+++ plugins/regexp_search/fitness/MultiobjectiveFitness.ml
@@ -12,52 +12,58 @@ let build_multiobjective_fitness json =
     let elim_size = json |> member "elim_size" |> to_int in
 
     (module struct
-        type t = int*int*float*float*int;; (** The fitness value contains in order, the number of true positive, the number of true negative, the partial match value of the positive examples, the partial match value of the negative examples and the size of the regexp. *)
+        type t = float*float*float*float*int;; (** The fitness value contains in order, the proportion of true positive, the proportion of true negative, the partial match value of the positive examples, the partial match value of the negative examples and the size of the regexp. *)
 
         type individual = RegexpTree.t
         type target_data = ExampleList.t
 
         let to_float (fullpos,fullneg,parpos,parneg,len) =
-            if len > elim_size then 0.
-            else
-                (float_of_int fullpos *. full_example_score) +. (float_of_int fullneg *. full_counter_example_score) +.
-                (parpos *. partial_example_score) +. (parneg *. partial_counter_example_score) +. (size_score /. (float_of_int len))
+            (fullpos *. full_example_score) +. (fullneg *. full_counter_example_score) +.
+            (parpos *. partial_example_score) +. (parneg *. partial_counter_example_score) +. (size_score /. (float_of_int len))
         ;;
 
         let to_string ((fullpos,fullneg,parpos,parneg,len) as f) =
-            Printf.sprintf "%.4f (full:%d+ %d- | par:%.2f+ %.2f- | len:%d)" (to_float f) fullpos fullneg parpos parneg len
+            Printf.sprintf "%.4f (full:%.2f%%+ %.2f%%- | char:%.2f%%+ %.2f%%- | len:%d)" (to_float f) (100. *. fullpos) (100. *. fullneg) (100. *. parpos) (100. *. parneg) len
         ;;
 
         let compare x y = Pervasives.compare (to_float x) (to_float y);;
 
         let compute (examples,counter_examples) regexp =
-            let automata = RegexpAutomata.from_tree regexp in
+            if RegexpTree.size regexp > elim_size then (0., 0., 0., 0., RegexpTree.size regexp)
+            else
+            (
+                let automata = RegexpAutomata.from_tree regexp in
 
-            let fullpos = ref 0 in
-            let fullneg = ref 0 in
-            let parpos = ref 0. in
-            let parneg = ref 0. in
+                let fullpos = ref 0 in
+                let fullneg = ref 0 in
+                let parpos = ref 0. in
+                let parneg = ref 0. in
 
-            let rec longest_interval = function
-                | [] -> 0
-                | (s,e)::t -> max (e-s) (longest_interval t)
-            in
+                let rec longest_interval = function
+                    | [] -> 0
+                    | (s,e)::t -> max (e-s) (longest_interval t)
+                in
 
-            let compute_example ex =
-                let matching_substrings = RegexpAutomata.matching_substrings automata ex in
-                parpos := !parpos +. (float_of_int (longest_interval matching_substrings) /. float_of_int (String.length ex));
-                if RegexpAutomata.is_matching automata ex then incr fullpos
-            in
-            Array.iter compute_example examples;
+                let compute_example ex =
+                    let matching_substrings = RegexpAutomata.matching_substrings automata ex in
+                    parpos := !parpos +. (float_of_int (longest_interval matching_substrings) /. float_of_int (String.length ex));
+                    if RegexpAutomata.is_matching automata ex then incr fullpos
+                in
+                Array.iter compute_example examples;
 
-            let compute_counter_example ex =
-                let matching_substrings = RegexpAutomata.matching_substrings automata ex in
-                parneg := !parneg +. 1. -. (float_of_int (longest_interval matching_substrings) /. float_of_int (String.length ex));
-                if not (RegexpAutomata.is_matching automata ex) then incr fullneg
-            in
-            Array.iter compute_counter_example counter_examples;
+                let compute_counter_example ex =
+                    let matching_substrings = RegexpAutomata.matching_substrings automata ex in
+                    parneg := !parneg +. 1. -. (float_of_int (longest_interval matching_substrings) /. float_of_int (String.length ex));
+                    if not (RegexpAutomata.is_matching automata ex) then incr fullneg
+                in
+                Array.iter compute_counter_example counter_examples;
 
-            (!fullpos, !fullneg, !parpos, !parneg, RegexpTree.size regexp)
+                (float_of_int !fullpos /. float_of_int (Array.length examples),
+                float_of_int !fullneg /. float_of_int (Array.length counter_examples),
+                !parpos /. float_of_int (Array.length examples),
+                !parneg /. float_of_int (Array.length counter_examples),
+                RegexpTree.size regexp)
+            )
         ;;
     end : EvolParams.Fitness with type individual = RegexpTree.t and type target_data = ExampleList.t)
 ;;
--- plugins/regexp_search/fitness/PartialMatchFitness.ml
+++ plugins/regexp_search/fitness/PartialMatchFitness.ml
@@ -5,6 +5,7 @@ let build_partial_match_fitness json =
     let open Yojson.Basic.Util in
     let example_score = json |> member "partial_example_score" |> to_float in
     let counter_example_score = json |> member "partial_counter_example_score" |> to_float in
+    let elim_size = json |> member "elim_size" |> to_int in
 
     (module struct
         type t = float*float;; (** The fitness value contains in order, the fitness value of the positive examples and the negative examples *)
@@ -15,27 +16,32 @@ let build_partial_match_fitness json =
         let to_float (pos,neg) = (pos *. example_score) +. (neg *. counter_example_score);;
 
         let to_string (pos,neg) =
-            Printf.sprintf "%.4f (%.2f+ %.2f-)" (to_float (pos,neg)) pos neg
+            Printf.sprintf "%.4f (%.2f%%+ %.2f%%-)" (to_float (pos,neg)) (100. *. pos) (100. *. neg)
         ;;
 
         let compare x y = Pervasives.compare (to_float x) (to_float y);;
 
         let compute (examples,counter_examples) regexp =
-            let automata = RegexpAutomata.from_tree regexp in
+            if RegexpTree.size regexp > elim_size then (0., 0.)
+            else
+            (
+                let automata = RegexpAutomata.from_tree regexp in
 
-            let rec longest_interval = function
-                | [] -> 0
-                | (s,e)::t -> max (e-s) (longest_interval t)
-            in
-            let compute_example_substring_score ex =
-                let matching_substrings = RegexpAutomata.matching_substrings automata ex in
-                float_of_int (longest_interval matching_substrings) /. float_of_int (String.length ex)
-            in
+                let rec longest_interval = function
+                    | [] -> 0
+                    | (s,e)::t -> max (e-s) (longest_interval t)
+                in
+                let compute_example_substring_score ex =
+                    let matching_substrings = RegexpAutomata.matching_substrings automata ex in
+                    float_of_int (longest_interval matching_substrings) /. float_of_int (String.length ex)
+                in
 
-            let positive_substr_match = Array.fold_left (fun score ex -> score +. (compute_example_substring_score ex)) 0. examples in
-            let negative_substr_match = Array.fold_left (fun score ex -> score +. (1. -. (compute_example_substring_score ex))) 0. counter_examples in
+                let positive_substr_match = Array.fold_left (fun score ex -> score +. (compute_example_substring_score ex)) 0. examples in
+                let negative_substr_match = Array.fold_left (fun score ex -> score +. (1. -. (compute_example_substring_score ex))) 0. counter_examples in
 
-            (positive_substr_match, negative_substr_match)
+                (positive_substr_match /. float_of_int (Array.length examples),
+                negative_substr_match /. float_of_int (Array.length counter_examples))
+            )
         ;;
     end : EvolParams.Fitness with type individual = RegexpTree.t and type target_data = ExampleList.t)
 ;;
